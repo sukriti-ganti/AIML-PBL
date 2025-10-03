e3cf2503dfbcf074e28e6347ed6d51d0fd44108a
Sukriti Ganti <sukriti.ganti.12@gmail.com>
2025-10-03 12:52:11 +0530

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+# models/__init__.py
+"""
+Expose model modules for easy import in main.py
+"""
+from . import conversion_model
+from . import funnel_model
+from . import lead_scoring_model
+# models/__init__.py
+"""
+Expose model modules for easy import in main.py
+"""
+from . import conversion_model
+from . import funnel_model
+from . import lead_scoring_model
